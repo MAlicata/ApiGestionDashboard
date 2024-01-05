eb986fc8f245818d2768062f8d2460fe8ea90380
Matias Alicata <matiasalicata@gmail.com>
2024-01-05 17:18:00 -0300

    add authentication and authorization

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+using ApiGestionDashboard.DataAccess;
+using ApiGestionDashboard.Entities;
+using ApiGestionDashboard.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
 namespace ApiGestionDashboard
 {
     public class Program
@@ -13,6 +22,34 @@ namespace ApiGestionDashboard
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContext<ApplicationDbContext>(option =>
+            {
+                option.UseSqlServer("name=DefaultConnection");
+            });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Ceo", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, Rol.Ceo.ToString());
+                });
+                options.AddPolicy("Gerente", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, Rol.Gerente.ToString());
+                });
+            });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            });
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWorkService>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -24,6 +61,7 @@ namespace ApiGestionDashboard
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
